aac647b8f310a4aa3111fbbba1a2284a54c261bb
Colt Xie <colt.xie@gmail.com>
2017-07-26 16:34:00 +0800

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -2,7 +2,7 @@
 name=$1
 if [ -z "$1" ]; then
     echo "usage $0 [container-name]"
-    echo "example: $0 douyu-php"
+    echo "example: $0 php"
     exit
 fi
 container_id=`docker ps -aql -f "name=$name"`
